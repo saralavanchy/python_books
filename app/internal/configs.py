b41f1ae6e9a1bb97ef96f99bf6c193b2ef14dc2d
sara lavanchy <saralavanchy94@gmail.com>
2022-05-10 12:45:51 -0300

    add violation

--- app/internal/configs.py
+++ app/internal/configs.py
@@ -1,12 +1,28 @@
 import datetime
 import jwt
 
-def generateJWT(userId):
+def getJWT(userId: number):
     timeLimit= datetime.datetime.utcnow() + datetime.timedelta(minutes=30)
     payload = {"user_id": userId ,"exp":timeLimit}
-    return jwt.encode(payload, "super-secret-6FDFBB8F-2909-4565-85EA-3F685784355E")
+    
+    return jwt.encode(payload, "super-secret-other-secret")
 
 configs = {
     "username":"sara",
     "password":"1234"
 }
+
+
+# Using of pickle/c_pickle/_pickle with load/loads:
+import pickle
+data = """ cos.system(S'dir')tR. """
+pickle.loads(data)
+
+###
+
+# Using PyYAML with load:
+import yaml
+document = "!!python/object/apply:os.system ['ipconfig']"
+print(yaml.load(document))
+
+AWS_SECRET_ACCESS_KEY = 'MjNjcnh1IDJieXJpdXYyeXJpaTJidnl1MnI4OXkyb3UwM'
